9f9927a0b6ed900a32332ff72049b7909b2cbc25
Coder591 <mazorchyk@gmail.com>
2018-10-24 17:22:16 +0300

    Work is done but need to fix quotes counting function in return

--- index.js
+++ index.js
@@ -30,7 +30,46 @@ const LoveQ = mongoose.model('LoveQ')
 const BusinQ= mongoose.model('BusinQ')
 const User  = mongoose.model('User')
 
-
+function QuoteCount() {
+   Quote.count({},function(err,count){
+   	 console.log("Quote count is "+count);
+     fs.writeFile("./files_to_help/QuoteNum.txt", count,"utf-8", function(err) {
+    					if(err) {return console.log(err);}				
+	 });
+    }); 
+    fs.readFile("./files_to_help/QuoteNum.txt",{encoding:'utf8'},(err,data)=>{
+             var ret=0+data;
+             return ret;
+           
+   });
+} 
+function LovequoteCount() {
+   LoveQ.count({},function(err,count){
+   	 console.log("LoveQ count is "+count);
+     fs.writeFile("./files_to_help/LoveQNum.txt", count,"utf-8", function(err) {
+    					if(err) {return console.log(err);}				
+	 });
+    }); 
+    fs.readFile("./files_to_help/LoveQNum.txt",{encoding:'utf8'},(err,data)=>{
+             var ret=0+data;
+             return ret;
+           
+   }); 
+} 
+function BizquoteCount() {
+   
+   BusinQ.count({},function(err,count){
+   	 console.log("BusinQ count is "+count);
+     fs.writeFile("./files_to_help/BusinQNum.txt", count,"utf-8", function(err) {
+    					if(err) {return console.log(err);}				
+	 });
+    }); 
+    fs.readFile("./files_to_help/BusinQNum.txt",{encoding:'utf8'},(err,data)=>{
+             
+             var ret=0+data;
+             return ret;
+   });
+} 
 
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id,"Look on keyboard",{
@@ -38,6 +77,7 @@ bot.onText(/\/start/, (msg) => {
             keyboard:[['/Quote'],['/Help'],['/Businessquote'],['/Lovequote']]
         }
     });
+    
 });
 
 bot.onText(/\/Quote/, (msg) => {
@@ -124,7 +164,7 @@ bot.onText(/\/Update/,(msg)=> {
 
     bot.sendMessage(msg.chat.id,"Look on Update keyboard",{
         	reply_markup:{
-            	keyboard:[['/UpdateParse'],['/DeleteQuote'],['/DeleteBusinQ'],['/DeleteLoveQ']]
+            	keyboard:[['/UpdateParse'],['/UpdateTxt']]
         	}
     	});
 });
@@ -133,10 +173,10 @@ bot.onText(/\/UpdateParse/,(msg)=> {
      bot.sendMessage(msg.chat.id," Look in source  code at link, what you Updating");
 
      request('https://motivationping.com/quotes/',(error,response,body)=>{
-       const $ = cheerio.load(body)
-       const wlinks = $('p');//.text();
+        const $ = cheerio.load(body)
+        const wlinks = $('p');//.text();
  
-        wlinks.each(i => {
+          wlinks.each(i => {
               var link = wlinks.eq(i).text();
        		  fs.writeFile("./files_to_help/"+i+".txt", link,"utf-8", function(err) {
     					if(err) {return console.log(err);}	
@@ -145,18 +185,80 @@ bot.onText(/\/UpdateParse/,(msg)=> {
 			  }); 
 			   
        
-         });
+          });
 
      })
   
+     bot.sendMessage(msg.chat.id,"quotes Are load to files\n Choose what you want to update",{
+        	reply_markup:{
+            	keyboard:[['/UpdateParseQuote'],['/UpdateParseBiz'],['/UpdateParseLove']]
+        	}
+    	});
+});
+
+bot.onText(/\/UpdateParseQuote/,(msg)=> {
+   var k=QuoteCount();
+   for (var j = 0; j < 380; j++) 
+  {
+  	fs.readFile("./files_to_help/"+j+".txt",{encoding:'utf8'},(err,data)=>{
+           data.split('\n').forEach(line=>{ 
+           	console.log(line)
+           	var line1=line+'\n';
+            //here we need to create new object in DB
+             Quote.create({ text:line1,id:k }, function (err, small) {
+       	  			if (err) return handleError(err);
+     	 	 })     
+     	  	k=k+1;    
+           })
+          
+  	})
+  }
+});
+bot.onText(/\/UpdateParseBiz/,(msg)=> {
+   var k=BizquoteCount();
+   for (var j = 0; j < 380; j++) 
+  {
+  	fs.readFile("./files_to_help/"+j+".txt",{encoding:'utf8'},(err,data)=>{
+           data.split('\n').forEach(line=>{ 
+           	console.log(line)
+           	var line1=line+'\n';
+            //here we need to create new object in DB
+             BusinQ.create({ text:line1,id:k }, function (err, small) {
+       	  			if (err) return handleError(err);
+     	 	 })     
+     	  	k=k+1;    
+           })
+          
+  	})
+  }
+});
+bot.onText(/\/UpdateParseLove/,(msg)=> {
+   //var k=LovequoteCount();
+  var k=8;
+  for (var j = 0; j < 380; j++) 
+  {
+  	fs.readFile("./files_to_help/"+j+".txt",{encoding:'utf8'},(err,data)=>{
+           data.split('\n').forEach(line=>{ 
+           	if (err) throw err;
+           	var LoveQuotTXTLine = line.toString();
+      
+      	LoveQ.create({ text:LoveQuotTXTLine,id:k }, function (err, small) {
+       	  if (err) return handleError(err);
+       	  console.log("LoveQ added")
+     	  })     
+     	  k=k+1;  
+           })
+          
+  	})
+  }
 });
 // make 3 commands by markup to update quote biz or love  
-bot.onText(/\/UpToDateBase/, (msg) => {
+/*bot.onText(/\/UpToDateBase/, (msg) => {
  
    //database.quotes.forEach(element => { new Quote(element).save() .catch(element=>console.log(element))});
    //Bizdatabase.businessquotes.forEach(element => { new BusinQ(element).save() .catch(element=>console.log(element))});
    //Lovedatabase.lovequotes.forEach(element => { new LoveQ(element).save() .catch(element=>console.log(element))});
-   /*console.log('Updating')
+   console.log('Updating')
   request('https://motivationping.com/quotes/',(error,response,body)=>{
        const $ = cheerio.load(body)
        const wlinks = $('p');//.text();
@@ -173,7 +275,7 @@ bot.onText(/\/UpToDateBase/, (msg) => {
        
          });
 
-    })*/
+    })
     console.log('end of parsing quotes and writing to file')
   for (var j = 0; j < 380; j++) 
   {
@@ -186,12 +288,22 @@ bot.onText(/\/UpToDateBase/, (msg) => {
   	})
   }
 
+}); */
+
+bot.onText(/\/UpdateTxt/,(msg)=> {
+	//need to add an instruction
+	bot.sendMessage(msg.chat.id,"Look on UpdateTxt keyboard",{
+        	reply_markup:{
+            	keyboard:[['/UpdateTxtQuote'],['/UpdateTxtBiznes'],['/UpdateTxtLove']]
+        	}
+    	});
 });
+
 
 bot.onText(/\/UpdateTxtQuote/, (msg) => {
      
      //var i=Quote.count({},function(err,count){console.log("Quote count is "+count)}); //28
-     var i=28;
+     var i=QuoteCount();
   	 fs.readFile(QuoteUrl, { encoding : 'utf8' },(err, data) => {
    		 if (err) throw err;
    		 data.split('\n').forEach(line => {
@@ -207,7 +319,7 @@ bot.onText(/\/UpdateTxtQuote/, (msg) => {
 bot.onText(/\/UpdateTxtBiznes/, (msg) => {
    
     //var j=BusinQ.count({},function(err,count){console.log("Biz count is "+count)}); //j=8;
-    var j=8;
+    var j=BizquoteCount();
   fs.readFile(BusinessUrl, { encoding : 'utf8' },(err, data) => {
      if (err) throw err;
      data.split('\n').forEach(line => {
@@ -223,7 +335,7 @@ bot.onText(/\/UpdateTxtBiznes/, (msg) => {
 bot.onText(/\/UpdateTxtLove/, (msg) => {
 
       // var k=LoveQ.count({},function(err,count){console.log("Love count is "+count)});//k=8;
-       var k=8;
+       var k=LovequoteCount();
   	  fs.readFile(LoveQuoteUrl, { encoding : 'utf8' },(err, data) => {
     	if (err) throw err;
    	 	data.split('\n').forEach(line => {
@@ -272,13 +384,14 @@ bot.onText(/\/DeleteBusinQ/,(msg)=>{
    	console.log("Removing control: "+J);
 });
 bot.onText(/\/DeleteLoveQ/,(msg)=>{
-     var K=LoveQ.count({},function(err,count){console.log("Love count is "+count)});//k=8;
-     for (var iiid = 0; iiid < 100; iiid++) {
+     LoveQ.count({},function(err,count){console.log("Love count is "+count)});//k=8;
+     
+     for (var iiid = 0; iiid < 1000; iiid++) {
   		LoveQ.find({id:iiid}).remove().then(_ =>console.log('Removed',iiid))
   	 }
   	 console.log(' All LoveQ removed');
 
-  	 console.log("Removing control: "+K);
+  	 LoveQ.count({},function(err,count){console.log("Love count is "+count)});//k=8;
 });
 // database useful  used methods
     //method used to delete any element by field
